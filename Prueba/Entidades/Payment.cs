c8ff17212eb8c2eea6556926d5cece2038ddb159
marcosguz <75583218+marcosguz@users.noreply.github.com>
2022-01-24 19:21:16 -0500

    Add files via upload

---
+++ Prueba/Entidades/Payment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entidades
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int StaffId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+        //Propiedad Relacion con Rental
+        public Rental RentalName { get; set; }
+        public int RentalId { get; set; }
+
+        //Propiedad Relacion con Customer
+        public Customer CustomerName { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
