c2e72b45ae6e2fd3038ef15e71d93b8ccc540e29
marcosguz <75583218+marcosguz@users.noreply.github.com>
2022-01-24 19:23:19 -0500

    Add files via upload

---
+++ ModeloDB/Class1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ModeloDB
+{
+    public class Class1: DbContext
+    {
+        //Configuracion de las entidades
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        //Modelado
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Relacion uno a uno de Rental a Customer
+            modelBuilder.Entity<Rental>()
+                .HasOne(rental => rental.CustomerName)
+                .WithMany(customer => customer.RentalName)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasForeignKey(rental => rental.CustomerId);
+
+            //Relacion uno a varios de Payment a Rental
+            modelBuilder.Entity<Payment>()
+                .HasOne(payment => payment.RentalName)
+                .WithMany(rental => rental.ListPayment)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasForeignKey(payment => payment.RentalId);
+
+            //Relacion uno a Varios de Payment a Customer
+            modelBuilder.Entity<Payment>()
+                .HasOne(payment => payment.CustomerName)
+                .WithMany(customer => customer.ListPayment)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasForeignKey(payment => payment.CustomerId);
+        }
+
+        //Conexion con la base de datos SQLServer
+        override protected void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("SERVER=DESKTOP-JEQTB22; initial catalog = Prueba; trusted_connection = true;");
+        }
+    }
+}
